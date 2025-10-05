bbf61ca76793ebdb3bf54d38d4ff60b4c305ebe1
Pearl761 <py9034a@american.edu>
2025-10-05 16:27:02 -0400

    finish

--- 06-fetch-apis/script.js
+++ 06-fetch-apis/script.js
@@ -5,7 +5,9 @@ const answersBox = document.querySelector('#answers');
 const pList = answersBox.querySelectorAll('p');
 
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', e => getAdviceAndActivity(e))
+
+async function getAdviceAndActivity(e) {
     e.preventDefault();
 
     const q = questionInput.value.trim();
@@ -17,6 +19,45 @@ form.addEventListener('submit', e => {
     }
 
     pList[0].textContent = 'Consulting the Book of Answers...';
+    pList[1].textContent = '';
+    pList[2].textContent = '';
+    pList[3].textContent = '';
     askBtn.disabled = true;
+
     
-})
+    await fetchAndShowAnswers();
+    
+    
+
+    askBtn.disabled = false;
+}
+
+async function fetchAndShowAnswers() {
+    const answerResp = await fetch("https://answerbook.david888.com/?lang=en");
+    const answerJson = await answerResp.json();
+    // const answerText = answerJson.answer;
+
+    let answerText = "";
+    if (typeof answerJson.answer === "string") {
+        answerText = answerJson.answer;
+    } else {
+        answerText = answerJson.answer.en; 
+    }
+
+    const actResp = await fetch("https://api.adviceslip.com/advice");
+    const actData = await actResp.json();
+    const activityText = actData.slip.advice;
+    
+    pList[0].innerHTML = '<strong>📜 Answer:</strong>';
+    pList[1].innerHTML = answerText;
+
+    pList[2].innerHTML = '<strong>🎲 Suggestion:</strong>';
+    pList[3].innerHTML = activityText;
+    
+}
+
+
+// fetch("https://answerbook.david888.com/?lang=en")
+//   .then(resp => resp.json())
+//   .then(data => console.log("Answerbook API response:", data))
+//   .catch(err => console.error("Answerbook API error:", err));
