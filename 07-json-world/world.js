b3ff9b0c6ed385b2cd531224af9cb6487f29722a
Pearl761 <py9034a@american.edu>
2025-10-11 16:35:41 -0400

    update

--- 07-json-world/world.js
+++ 07-json-world/world.js
@@ -2,17 +2,18 @@ import fs from "fs";
 
 let world;
 try{
-    let worldjson = fs.readFileSync("world.json", "utf-8");
+    let worldjson = fs.readFileSync("world.json", "utf8");
     world = JSON.parse(worldjson);
 }catch(error){
     console.log(error);
 }
 
+let n = 0
 for (let i = 0; i < world.regions.length; i++){
     for (let j = 0; j < world.regions[i].towns.length; j++){
-        // console.log(world.regions[i].towns[j].name, ":"); 
-        console.log(`${world.regions[i].towns[j].name}:`);
-        console.log(`population: ${world.regions[i].towns[j].population}`);
+        n++;
+        console.log(`Town${n}: ${world.regions[i].towns[j].name}`);
+        console.log(`Population: ${world.regions[i].towns[j].population}`);
     }
 }
 
